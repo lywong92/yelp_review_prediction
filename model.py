f1aa8df8e8b5361220f1fc7ba707b59a015e2db8
Kushal D'Souza <kdsouza@usc.edu>
2019-11-17 19:06:36 -0800

    Updated model

--- model.py
+++ model.py
@@ -7,7 +7,6 @@ from sklearn import preprocessing
 data_file = "normalised_data.json"
 with open(data_file, 'r') as f:
     full_data = json.load(f)
-    print(full_data['useful'][0])
-    df = pd.DataFrame(full_data['useful'])
+    df = pd.DataFrame(json.loads(full_data['useful']))
     
 print("dataset size: {0:d} rows x {1:d} columns".format(df.shape[0], df.shape[1]))
